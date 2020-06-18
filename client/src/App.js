5ca72286ff392bfb40d6440ccf623485de25e22d
Sai Kiran <vepamani.saikiran@zapcg.com>
2020-06-18 13:28:17 +0530

    npx create-react-app client, npm i react-router-dom,navbar

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import NavBar from './components/Navbar'
+import Home from './components/screens/Home'
+import Signin from './components/screens/Login'
+import Profile from './components/screens/Profile'
+import Signup from './components/screens/Signup'
+import "./App.css"
+import {BrowserRouter,Route} from 'react-router-dom'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+     <NavBar />
+     <Route exact path = "/">
+        <Home/>
+     </Route>
+     <Route path = "/signin">
+        <Signin/>
+     </Route>
+     <Route path = "/profile">
+        <Profile/>
+     </Route>
+     <Route path = "/signup">
+        <Signup/>
+     </Route>
+    </BrowserRouter>
+  );
+}
+
+export default App;
