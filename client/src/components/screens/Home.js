7c0c3d47e9e9a9ec9b1855ae70c3f6bcf06c09e3
Sai Kiran <vepamani.saikiran@zapcg.com>
2020-06-23 16:56:51 +0530

    home page static data added

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -2,7 +2,78 @@ import React from 'react'
 
 const Home = ()=>{
     return (
-        <h1>home</h1>
+        <div className="home">
+            <div className="card home-card">
+            <h5>
+                Sai Kiran
+            </h5>
+            <div className="card-image">
+                <img src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=752&q=80" />
+            </div>
+            <div className="card-content">
+            <i className="material-icons" style={{color:"red"}}>favorite</i>  
+            <h6>tile</h6>
+            <p>this a post</p>
+            <input type="text" placeholder="add a comment" />
+            </div>
+            </div>
+            <div className="card home-card">
+            <h5>
+                Sai Kiran
+            </h5>
+            <div className="card-image">
+                <img src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=752&q=80" />
+            </div>
+            <div className="card-content">
+            <i className="material-icons" style={{color:"red"}}>favorite</i>  
+            <h6>tile</h6>
+            <p>this a post</p>
+            <input type="text" placeholder="add a comment" />
+            </div>
+            </div>
+            <div className="card home-card">
+            <h5>
+                Sai Kiran
+            </h5>
+            <div className="card-image">
+                <img src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=752&q=80" />
+            </div>
+            <div className="card-content">
+            <i className="material-icons" style={{color:"red"}}>favorite</i>  
+            <h6>tile</h6>
+            <p>this a post</p>
+            <input type="text" placeholder="add a comment" />
+            </div>
+            </div>
+            <div className="card home-card">
+            <h5>
+                Sai Kiran
+            </h5>
+            <div className="card-image">
+                <img src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=752&q=80" />
+            </div>
+            <div className="card-content">
+            <i className="material-icons" style={{color:"red"}}>favorite</i>  
+            <h6>tile</h6>
+            <p>this a post</p>
+            <input type="text" placeholder="add a comment" />
+            </div>
+            </div>
+            <div className="card home-card">
+            <h5>
+                Sai Kiran
+            </h5>
+            <div className="card-image">
+                <img src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=752&q=80" />
+            </div>
+            <div className="card-content">
+            <i className="material-icons" style={{color:"red"}}>favorite</i>  
+            <h6>tile</h6>
+            <p>this a post</p>
+            <input type="text" placeholder="add a comment" />
+            </div>
+            </div>
+        </div>
     )
 }
 
